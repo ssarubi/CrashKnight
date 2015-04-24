bfd4b75481aa04b7e7bd4447a834d25c6f17cba7
Ssarubi <pakk2040@naver.com>
2015-04-24 09:15:51 +0900

    Setting

--- app/src/main/java/bayaba/game/basic/GameMain.java
+++ app/src/main/java/bayaba/game/basic/GameMain.java
@@ -21,54 +21,97 @@ public class GameMain
 	public GameInfo gInfo; // 게임 환경 설정용 클래스 : MainActivity에 선언된 것을 전달 받는다.
 	public float TouchX, TouchY;
 
+    public int DPTimer = 0;
+
     Font font = new Font();
+    Font DP = new Font();
 
     Sprite backSpr = new Sprite();
     Sprite clothSpr = new Sprite();
 
-    Sprite ratSpr = new Sprite();
 
-    ArrayList<GameObject>Game = new ArrayList<GameObject>();
+    Sprite ratSpr = new Sprite();
+    Sprite crabSpr = new Sprite();
 
     GameObject Stage = new GameObject();
     GameObject Hero = new GameObject();
-    GameObject Monster = new GameObject();
+    ArrayList<GameObject>Monster = new ArrayList<GameObject>();
+
+    public void Stageinit(){
+        GameObject temp = new GameObject();
+
+        if(Stage.state >= 1){
+            temp.SetObject(ratSpr, 0, 0, 440, 400, 0, 0);
+            temp.mx1 = -1;
+            temp.energy = 100;
+            temp.damage = 5;
+            temp.AddFrameLoop(0.2f);
+            Monster.add(temp);
+        }
+        if(Stage.state >= 2){
+            temp.SetObject(crabSpr, 0, 0, 450, 400, 0, 0);
+            temp.mx1 = -1;
+            temp.energy = 100;
+            temp.damage = 5;
+            temp.AddFrameLoop(0.2f);
+            Monster.add(temp);
+
+        }
+    }
+
 
 
     public void DrawFont(){
-        font.LoadFont(MainContext, "Hoon.ttf");
         font.BeginFont(gInfo);
-        font.DrawColorFont(mGL, 30, 30, 255, 0, 0, 10, "Hero Life : "+String.valueOf(Hero.energy));
-        font.DrawColorFont(mGL, 30, 60, 0, 0, 255, 10, "Hero Damage : "+String.valueOf(Hero.damage));
-        font.DrawColorFont(mGL, 350, 30, 255, 0, 0, 10, "Hero Life : "+String.valueOf(Monster.energy));
-        font.DrawColorFont(mGL, 350, 60, 0, 0, 255, 10, "Hero Damage : "+String.valueOf(Monster.damage));
+        font.LoadFont(MainContext, "HMKLP.TTF");
+        font.DrawFontCenter(mGL, gInfo, 240, 170, 250, 250, 250, 30, "Stage " + String.valueOf(Stage.state));
+        font.DrawFont(mGL, 30, 30, 15, "Hero Life : " + String.valueOf(Hero.energy));
+        font.DrawFont(mGL, 30, 60, 15, "Hero Damage : " + String.valueOf(Hero.damage));
         font.EndFont(gInfo);
+    }
+
+    public void DamagePanel(){
+        if(DPTimer > 0){
+            DP.BeginFont(gInfo);
+            DP.DrawFontCenter(mGL, gInfo, Stage.x, Stage.y, 0, 0, 0, 10, String.valueOf(Hero.damage));
+            DP.EndFont(gInfo);
+            Stage.y--;
+        }
     }
 
     public void Crash(){
 
-        if(gInfo.CrashCheck(Hero, Monster, 0, 0)){
-            Hero.speed = -5;
-            Monster.speed = 5;
-            LifeCheck();
+        for(int i = 0; i < Monster.size(); i++){
+            if(gInfo.CrashCheck(Hero, Monster.get(i), 0, 0)){
+                Hero.speed = -5;
+                Monster.get(i).speed = 5;
+
+                LifeCheck(Monster.get(i));
+            }
         }
 
+        for(int i = 0; i < Monster.size(); i++){
+            if(Monster.get(i).speed > 0) Monster.get(i).speed -= Math.random()/5;
+        }
         if(Hero.speed < 0) Hero.speed = Hero.speed * 0.95f;
-        if(Monster.speed > 0) Monster.speed -= Math.random()/5;
-
+        if(DPTimer > 0) DPTimer--;
     }
 
-    public void LifeCheck(){
+    public void LifeCheck(GameObject Monster){
         Hero.energy -= Monster.damage;
         Monster.energy -= Hero.damage;
 
-        if(Hero.energy < 0){
+        if(Hero.energy <= 0){
             // 패배처리
         }
-        if(Monster.energy < 0){
-            Log.d("현재 Stage : ", String.valueOf(Stage.state));
-            Stage.state++;
+        if(Monster.energy <= 0){
+            Monster.dead = true;
         }
+
+        DPTimer = 50;
+        Stage.x = Monster.x;
+        Stage.y = Monster.y-25;
+
     }
 
 	public GameMain( Context context, GameInfo info ) // 클래스 생성자 (메인 액티비티에서 호출)
@@ -84,19 +127,19 @@ public class GameMain
         clothSpr.LoadSprite(mGL, MainContext, "clotharmor.spr");
 
         ratSpr.LoadSprite(mGL, MainContext, "rat.spr");
+        crabSpr.LoadSprite(mGL, MainContext, "crab.spr");
 
         Hero.SetObject(clothSpr, 0, 0, 40, 400, 0, 0);
-        Monster.SetObject(ratSpr, 0, 0, 440, 400, 0, 0);
 
         // 초기 데이터 세팅
         Stage.state = 1;
-        Hero.mx1 = 1;
-        Monster.mx1= -1;
 
-        Hero.energy = 100;
-        Monster.energy = 100;
+        Hero.mx1 = 1;
+        Hero.energy = 1000;
         Hero.damage = 20;
-        Monster.damage = 10;
+
+        Stageinit();
+
     }
 	
 	public void PushButton( boolean push ) // OpenGL 화면에 터치가 발생하면 GLView에서 호출된다.
@@ -115,14 +158,27 @@ public class GameMain
             Hero.DrawSprite(gInfo);
             Hero.AddFrameLoop(0.2f);
 
-            Monster.x += Monster.speed + Monster.mx1;
-            Monster.DrawSprite(gInfo);
-            Monster.AddFrameLoop(0.2f);
 
+            for(int i = 0; i < Monster.size(); i++){
+                Monster.get(i).x += Monster.get(i).speed + Monster.get(i).mx1;
+                Monster.get(i).DrawSprite(gInfo);
+                if(Monster.get(i).dead) Monster.remove(i);
+            }
 
+            if(Stage.timer > 60){
+                Log.d("Monster Size : ", String.valueOf(Monster.size()));
+                Stage.timer = 0;
+            }
+                Stage.timer++;
 
             Crash();
             DrawFont();
+            DamagePanel();
+
+            if(Monster.size() == 0){
+                Stage.state++;
+                Stageinit();
+            }
 		}
 	}
 }
